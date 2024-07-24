3656d47b08a3c5f5a133435f5be5531b2e6548a1
Hamidou Woury Ba <hamidouwouryba@esp.sn>
2024-07-24 16:40:06 +0000

    Ajout de la navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
         brightRedLight: 'hsl(12, 88%, 69%)',
         brightRedSupLight: 'hsl(12, 80%, 95%)',
         darkBlue: 'hsl(228, 39%, 23%)',
-        darlGrayIshBlue: 'hsl(227, 12%, 61%)',
+        darkGrayIshBlue: 'hsl(227, 12%, 61%)',
         veryDarkBlue: 'hsl(223, 13%, 13%)',
         VeryPaleRed: 'hsl(13, 100%, 96%)',
         veryLightGray: 'hsl(0, 0%, 98%)',
